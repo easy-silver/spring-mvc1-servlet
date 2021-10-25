1b75b55f2729e8cec61f12ec9e1a27df9100ab8c
Timo <developer.timo@gmail.com>
2021-10-25 22:17:59 +0900

    [서블릿, JSP, MVC 패턴] 서블릿으로 회원 관리 웹 애플리케이션 만들기 - 생성자 롬복 어노테이션으로 변경

--- src/main/java/hello/servlet/domain/member/MemberRepository.java
+++ src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -11,16 +11,13 @@ import java.util.Map;
 /**
  * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
  */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
 
     private static final MemberRepository instance = new MemberRepository();
-
-    private MemberRepository() {
-
-    }
 
     public static MemberRepository getInstance() {
         return instance;
